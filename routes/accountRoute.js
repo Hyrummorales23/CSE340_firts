25079f6e58e43b4aa8b742371b7e19bd8de344f2
Hyrummorales23 <hyrummorales23@gmail.com>
2025-05-28 19:28:26 -0600

    server side data validation working correctly

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,12 +3,21 @@ const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities/")
+const regValidate = require("../utilities/account-validation")
 
 // Route to build the login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
 
 // Route to build the registration view
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
+
+// Route for data validation before processing registration
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  )
 
 //Route to process registration
 router.post(
